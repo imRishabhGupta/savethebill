3a9dcfced9260e05a58d29b30f6985a2de9d5fc3
imRishabhGupta <rishabhgpt86@gmail.com>
2016-03-29 15:32:12 +0530

    Sign in added

--- app/src/main/java/com/example/user/savethebill/ScrollingActivity.java
+++ app/src/main/java/com/example/user/savethebill/ScrollingActivity.java
@@ -8,11 +8,19 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.firebase.client.AuthData;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 
-public class ScrollingActivity extends AppCompatActivity {
+import java.util.HashMap;
+import java.util.Map;
+
+public class ScrollingActivity extends AppCompatActivity  {
 Firebase firebase;
+    EditText email,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,7 +38,81 @@ Firebase firebase;
         });
         Firebase.setAndroidContext(this);
         firebase = new Firebase("https://savethebill.firebaseio.com");
+        firebase.addAuthStateListener(new Firebase.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(AuthData authData) {
+                if (authData != null) {
+                    Toast.makeText(getApplicationContext(),"You  are logged in",Toast.LENGTH_SHORT);
+                } else {
+                    Toast.makeText(getApplicationContext(),"Sorry!!",Toast.LENGTH_SHORT);
 
+                    }
+            }
+        });
+
+    }
+
+    public void submit(View view){
+        email=(EditText)findViewById(R.id.email);
+        password=(EditText)findViewById(R.id.password);
+        String emailText=email.getText().toString();
+        String passwordText=password.getText().toString();
+        firebase.authWithPassword(emailText, passwordText,
+                new Firebase.AuthResultHandler() {
+                    @Override
+                    public void onAuthenticated(AuthData authData) {
+                        // Authentication just completed successfully :)
+                        Map<String, String> map = new HashMap<String, String>();
+                        map.put("provider", authData.getProvider());
+                        if (authData.getProviderData().containsKey("displayName")) {
+                            map.put("displayName", authData.getProviderData().get("displayName").toString());
+                        }
+                        firebase.child("users").child(authData.getUid()).setValue(map);
+                    }
+
+                    @Override
+                    public void onAuthenticationError(FirebaseError error) {
+                        switch (error.getCode()) {
+                            case FirebaseError.USER_DOES_NOT_EXIST:
+                                // handle a non existing user
+                                break;
+                            case FirebaseError.INVALID_PASSWORD:
+                                // handle an invalid password
+                                break;
+                            default:
+                                // handle other errors
+                                break;
+                        }
+                        // Something went wrong :(
+                    }
+                });
+    }
+    public  void logIn(View view){
+        email=(EditText)findViewById(R.id.email);
+        password=(EditText)findViewById(R.id.password);
+        String emailText=email.getText().toString();
+        String passwordText=password.getText().toString();
+
+        Firebase ref = new Firebase("https://savethebill.firebaseio.com");
+// Create a handler to handle the result of the authentication
+        Firebase.AuthResultHandler authResultHandler = new Firebase.AuthResultHandler() {
+            @Override
+            public void onAuthenticated(AuthData authData) {
+                // Authenticated successfully with payload authData
+            }
+            @Override
+            public void onAuthenticationError(FirebaseError firebaseError) {
+                // Authenticated failed with error firebaseError
+            }
+        };
+// Authenticate users with a custom Firebase token
+        ref.authWithCustomToken("<token>", authResultHandler);
+// Alternatively, authenticate users anonymously
+        ref.authAnonymously(authResultHandler);
+// Or with an email/password combination
+        ref.authWithPassword(emailText,passwordText, authResultHandler);
+// Or via popular OAuth providers ("facebook", "github", "google", or "twitter")
+        ref.authWithOAuthToken("<provider>", "<oauth-token>", authResultHandler);
     }
 
     @Override
